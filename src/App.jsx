3aaec414c62eba54b9d7c7aa7395319dba8614d5
Saint Lord <thesaint@Saints-MacBook-Pro.local>
2023-05-14 03:22:46 +0600

    React router, express, mongodb, tailwind, daisy installed and configured, and needed component+route created.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-4xl text-violet-500 text-center mt-14'>Mongo Coffee Store</h1>
+      
+    </>
+  )
+}
+
+export default App
